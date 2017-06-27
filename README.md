27ccd5b1d135048eb7d9e1211f39f04d32e2de6d
Alex Rodionov <p0deje@gmail.com>
2017-06-27 12:42:34 +0600

    Initial commit

---
+++ README.md
@@ -0,0 +1,10 @@
+# se-java-example
+
+I kept finding out it is hard for me to figure out how to use Selenium in Java for
+people who don't know Java and has no IDE, so I made this small example.
+
+## How To Use
+
+1. Clone the repo.
+2. Add your code to `AppTest.java`.
+3. Run `mvn test`.
---
+++ src/test/java/com/example/se/AppTest.java
@@ -0,0 +1,17 @@
+package com.example.se;
+
+import junit.framework.Test;
+import junit.framework.TestCase;
+import junit.framework.TestSuite;
+
+import org.openqa.selenium.WebDriver;
+import org.openqa.selenium.firefox.FirefoxDriver;
+
+public class AppTest extends TestCase {
+    public void testApp() {
+        // Put your code here:
+        //     WebDriver driver = new FirefoxDriver(binary, profile, caps);
+        //     driver.get("http://www.google.com");
+        //     driver.quit();
+    }
+}
